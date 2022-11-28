3a8682244a137b0dc054611d069b3909b55da125
Girish9084 <girish0238.be20@chitkara.edu.in>
2022-11-28 18:42:44 +0530

    first

---
+++ src/components/data/Data.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DataStyles.css'
+
+const Data = () => {
+    return (
+        <div className='data'>
+            <div className="container">
+                <div className="content">
+                    <h2><span>About</span> Recovery</h2>
+                    <p>Most people have experienced the disappointment of losing an
+                         important file, and fortunately, one doesn't have to be an expert
+                          to get it back.</p>
+                    <div>
+                        {/* <button>Sign Up</button> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Data
---
+++ src/components/recovery/Recovery.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './RecoveryStyles.css'
+
+const Recovery = () => {
+    return (
+        <div className='recovery'>
+            <div className="container">
+                <div className="content">
+                    <h2><span>Data</span> Recovery</h2>
+                    <p>Most people have experienced the disappointment of losing an important file,
+                         and fortunately, one doesn't have to be an expert to get it back.</p>
+                <div>
+                    {/* <button>Learn More</button> */}
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Recovery
